0ace9cd46efe656899b0e9b6728e53f92c824f82
Alexandr Timofeev <taratatuy@yandex.ru>
2021-06-09 23:23:17 +0000

    Add sticky sidebar.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,19 @@
-console.log('Console works!')
+function setStickyObserver() {
+    function showSticky(e) {
+        console.log(e[0].intersectionRatio)
+        e[0].target.classList.toggle('_showed', e[0].intersectionRatio != 0)
+    }
+
+    const options = {
+        rootMargin: '0px 0px -90%  0px',
+        threshold: 0,
+    }
+
+    const obs = new IntersectionObserver(showSticky, options)
+
+    const plate = document.querySelector('.sidebar__sticky-card')
+
+    obs.observe(plate)
+}
+
+setStickyObserver()
